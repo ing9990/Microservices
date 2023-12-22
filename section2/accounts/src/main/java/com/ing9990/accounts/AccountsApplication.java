7cbe288284d68053724d552a1a859c398a2a9ece
ing9990 <mail80782258@gmail.com>
2023-12-22 15:00:18 +0900

    Sec2-30 Assignment to build Loans & Card Microservices

--- section2/accounts/src/main/java/com/ing9990/accounts/AccountsApplication.java
+++ section2/accounts/src/main/java/com/ing9990/accounts/AccountsApplication.java
@@ -1,9 +1,31 @@
 package com.ing9990.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Accounts microservice REST API Documentation",
+        description = "EazyBank Accounts microservice REST API Documentation",
+        version = "v1",
+        contact = @Contact(
+            name = "Taewoo Kim",
+            email = "mail80782258@gmail.cokm",
+            url = "https://www.ing9990.xyz"
+        )
+    ),
+    externalDocs = @ExternalDocumentation(
+        description =  "EazyBank Accounts microservice REST API Documentation",
+        url = "https://www.ing9990.xyz"
+    )
+)
 public class AccountsApplication {
 
     public static void main(String[] args) {
--- section2/accounts/src/main/java/com/ing9990/accounts/repository/CustomerRepository.java
+++ section2/accounts/src/main/java/com/ing9990/accounts/repository/CustomerRepository.java
@@ -1,8 +1,12 @@
 package com.ing9990.accounts.repository;
 
 import com.ing9990.accounts.entity.Customer;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
+    @Query("select c from Customer c where c.mobileNumber = ?1")
+    Optional<Customer> findByMobileNumber(String mobileNumber);
 }
