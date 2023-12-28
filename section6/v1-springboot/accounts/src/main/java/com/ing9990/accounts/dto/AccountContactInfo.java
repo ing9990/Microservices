c0eef0642c0c95e560915148befe151636e30810
ing9990 <mail80782258@gmail.com>
2023-12-28 14:45:08 +0900

    Sec6-68 Reading configurations using @ConfigurationProperties

---
+++ section6/v1-springboot/accounts/src/main/java/com/ing9990/accounts/dto/AccountContactInfo.java
@@ -0,0 +1,10 @@
+package com.ing9990.accounts.dto;
+
+import java.util.List;
+import java.util.Map;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "accounts")
+public record AccountContactInfo(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+
+}
