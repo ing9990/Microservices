8286d5f419897a536e6d2ccd30b3e629a0893a63
ing9990 <mail80782258@gmail.com>
2024-01-03 13:38:02 +0900

    Sec6-86 Refresh config at runtime using Spring Cloud Bus & Spring Cloud Monitor

--- section6/v2-spring-cloud-config/cards/src/main/java/com/ing9990/cards/controller/CardsController.java
+++ section6/v2-spring-cloud-config/cards/src/main/java/com/ing9990/cards/controller/CardsController.java
@@ -1,6 +1,9 @@
 package com.ing9990.cards.controller;
 
+import static org.springframework.http.HttpStatus.OK;
+
 import com.ing9990.cards.constants.CardsConstants;
+import com.ing9990.cards.dto.CardsContactInfoDto;
 import com.ing9990.cards.dto.CardsDto;
 import com.ing9990.cards.dto.ErrorResponseDto;
 import com.ing9990.cards.dto.ResponseDto;
@@ -14,6 +17,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -26,11 +30,12 @@ import org.springframework.web.bind.annotation.*;
 )
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
-@AllArgsConstructor
 @Validated
+@RequiredArgsConstructor
 public class CardsController {
 
-    private ICardsService iCardsService;
+    private final CardsContactInfoDto cardContractInfo;
+    private final ICardsService iCardsService;
 
     @Operation(
             summary = "Create Card REST API",
@@ -157,4 +162,21 @@ public class CardsController {
         }
     }
 
+    @ApiResponses({
+        @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status OK"
+        ),
+        @ApiResponse(
+            responseCode = "500",
+            description = "HTTP Status Internal Server Error",
+            content = @Content(
+                schema = @Schema(implementation = ErrorResponseDto.class)
+            )
+        )
+    })
+    @GetMapping("/contact-info")
+    public ResponseEntity<CardsContactInfoDto> getContactInfo(){
+        return ResponseEntity.status(OK).body(cardContractInfo);
+    }
 }
